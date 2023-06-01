55543a4b2425aed9fd75f639c5434b6f253f8a19
HEEKWON <higun295@gmail.com>
2023-06-01 16:13:30 +0900

    20230601_3

---
+++ XAMARIN_TOYS/XAMARIN_TOYS/Units/IconTextButton.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
+using Xamarin.Forms.Xaml;
+
+namespace XAMARIN_TOYS.Units
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class IconTextButton : ContentView
+    {
+        public IconTextButton()
+        {
+            InitializeComponent();
+
+
+
+            //icon.Data = IconPath;
+            //text.Text = Text;
+
+            //var tab = new TapGestureRecognizer();
+            //tab.Tapped += (s, e) =>
+            //{
+            //    frame.BackgroundColor = Color.LightGray;
+            //};
+            ////tab.
+
+            ////frame.GestureRecognizers.Add(tab);
+        }
+
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create
+        (
+            nameof(CornerRadius),
+            typeof(float),
+            typeof(IconTextButton),
+            default(float),
+            BindingMode.OneWay
+        );
+
+        public float CornerRadius
+        {
+            get { return (float)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            base.OnPropertyChanged(propertyName);
+
+            if (propertyName == CornerRadiusProperty.PropertyName)
+            {
+                frame.CornerRadius = CornerRadius;
+            }
+        }
+
+
+
+
+
+
+
+
+
+        //public static readonly BindableProperty IconProperty = BindableProperty.Create(nameof(IconPath), typeof(Geometry), typeof(IconTextButton), default(string), BindingMode.OneWay);
+        //[TypeConverter(typeof(PathGeometryConverter))]
+        //public Geometry IconPath
+        //{
+        //    get { return (Geometry)GetValue(IconProperty); }
+        //    set { SetValue(IconProperty, value); }
+        //}
+
+        //public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(IconTextButton), default(string), BindingMode.OneWay);
+        //public string Text
+        //{
+        //    get { return (string)GetValue(TextProperty); }
+        //    set { SetValue(TextProperty, value); }
+        //}
+
+        //protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    base.OnPropertyChanged(propertyName);
+
+        //    if (propertyName == IconProperty.PropertyName)
+        //    {
+        //        icon.Data = IconPath;
+        //    }
+        //    else if (propertyName == TextProperty.PropertyName)
+        //    {
+        //        text.Text = Text;
+        //    }
+        //}
+    }
+}
--- XAMARIN_TOYS/XAMARIN_TOYS/obj/Debug/netstandard2.0/Views/LoginPage.xaml.g.cs
+++ XAMARIN_TOYS/XAMARIN_TOYS/obj/Debug/netstandard2.0/Views/LoginPage.xaml.g.cs
@@ -17,12 +17,8 @@ namespace XAMARIN_TOYS.Views {
     public partial class LoginPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.StackLayout MainLayout;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(LoginPage));
-            MainLayout = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "MainLayout");
         }
     }
 }
